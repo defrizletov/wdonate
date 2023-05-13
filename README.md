617bf12cb4910ff543d5576e6d14c42025667d5f
defriz <91366636+defrizletov@users.noreply.github.com>
2023-05-13 10:53:32 +0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,48 @@
+<p align="center"><img src="https://raw.githubusercontent.com/defrizletov/wdonate/main/docs/logo.svg?sanitize=true"></p>
+<p align="center">
+<a href="https://www.npmjs.com/package/wdonate"><img src="https://img.shields.io/npm/v/wdonate.svg?style=flat-square" alt="NPM version"></a>
+<a href="https://www.npmjs.com/package/wdonate"><img src="https://img.shields.io/npm/dt/wdonate.svg?style=flat-square" alt="NPM downloads"></a>
+</p>
+
+<div align="center">
+
+**Модуль** для удобного использования **[Wdonate API](https://www.wdonate.ru/documentation.html)**.
+  
+От **[AdepT-Hub](https://adept-hub.ru)** с ❤.
+
+</div>
+
+## 📦 Установка
+
+```sh
+npm i wdonate
+```
+
+<br/>
+
+## 🚀 Использование
+
+```js
+const { WDonate, WDonateCallback } = require('wdonate');
+
+// token - ваш ключ авторизации, полученный в приложении.
+// botId - VK ID вашего бота.
+const wDonate = new WDonate({ token: process.env.TOKEN, botId: process.env.BOT_ID });
+
+// Запрос для примера, получение баланса нашего аккаунта.
+// Потом вывод ответа или ошибки в консоль.
+wDonate.getBalance().then(console.log).catch(console.error);
+
+// Подключение callback.
+
+// Создаем сервер с нашим callback_secret.
+const wDonateCallback = new WDonateCallback(process.env.CALLBACK_SECRET);
+
+// Устанавливаем обработчик, который выполнится, когда придёт пополнение.
+// Эта функция выведет объект события в консоль.
+wDonateCallback.on(event => console.log(event));
+
+// Запуск сервера на порт 3000 и хост localhost (порт 3000 стоит по умолчанию).
+// Потом, если все успешно, пишем в консоль, что сервер запустился, в противном случае выводим ошибку в консоль.
+wDonateCallback.start(3000, 'localhost').then(console.log('WDonate Callback has been started.')).catch(console.error);
+```
---
+++ index.js
@@ -0,0 +1,138 @@
+const { post } = require('axios');
+
+exports.WDonateCallback = class {
+    constructor (botToken) {
+        if(!botToken) throw new Error('WDonate Callback: Вы не передали ключ авторизации (token).');
+    
+        this.botToken = botToken;
+
+        this.callbacks = [];
+
+        this.fastify = new require('fastify')();
+
+        this.fastify.post('/', request => {
+            const { body } = request, { type, payment } = body;
+
+            if(!this.#check(body)) return { status: 'failed' };
+
+            if(type === 'payment' && payment) this.callbacks.map(callback => callback(payment));
+
+            return { status: 'success', ...(type === 'seturl' ? { token: this.botToken } : {}) };
+        });
+    };
+
+    #check (body) {
+        const { sign } = body;
+
+        return (require('js-md5')(['botId','type','date'].reduce((_, key) => (_ + `:${body[key]}`), '') + `:${this.botToken}`) === sign);
+    };
+
+    on (callback) {
+        if(typeof callback !== 'function') throw new Error('WDonate Callback: Callback must be a function.');
+
+        this.callbacks.push(callback);
+    };
+
+    /**
+    * @returns {Promise}
+    */
+    start (port = 3000, host = '0.0.0.0') {
+        return this.fastify.listen({ port, host });
+    };
+
+    /**
+    * @returns {Promise}
+    */
+    stop () {
+        return this.fastify.close();
+    };
+};
+
+exports.WDonate = class {
+    constructor (options = {}) {
+        const { token, botId } = options;
+
+        if(!token || !botId) throw new Error(`WDonate: Вы не передали ${token ? 'VK ID бота (botId)' : 'ключ авторизации (token)'}.`);
+
+        this.api = {
+            token,
+            botId,
+            url: 'https://wdonate.ru/api/'
+        };
+    };
+
+    /**
+    * @name callApi
+    * @description Метод для вызова API.
+    * @param {string} method - Вызываемый метод.
+    * @param {object} data - Параметры запроса.
+    * @returns {Promise}
+    */
+    callApi (method, data = {}) {
+        const { url, token, botId } = this.api;
+
+        return post(url + method, { token, botId, ...data }).then(({ data: { response } }) => response);
+    };
+
+    /**
+    * @name getLink
+    * @description Метод для создания новой ссылки на оплату.
+    * @param {number} userId - ID пользователя.
+    * @param {number} sum - Сумма по умолчанию.
+    * @param {number} payload - Произвольное число.
+    * @see https://www.wdonate.ru/documentation.html
+    */
+    getLink (userId = 0, sum = 0, payload = 0) {
+        return this.callApi('getLink', { userId, sum, payload });
+    };
+
+    /**
+    * @name getPayments
+    * @description Метод для получения истории платежей.
+    * @param {number} count - Лимит записей.
+    * @see https://www.wdonate.ru/documentation.html
+    */
+    getPayments (count = 10) {
+        return this.callApi('getPayments', { count });
+    };
+
+    /**
+    * @name getBalance
+    * @description Метод для получения текущего баланса.
+    * @see https://www.wdonate.ru/documentation.html
+    */
+    getBalance () {
+        return this.callApi('getBalance');
+    };
+
+    /**
+    * @name getCallback 
+    * @description Метод для получения текущего Callback URL.
+    * @see https://www.wdonate.ru/documentation.html
+    * @see https://wdonate.ru/callback.html
+    */
+    getCallback () {
+        return this.callApi('getCallback');
+    };
+
+    /**
+    * @name setCallback
+    * @description Метод для установки Callback URL.
+    * @param {string} callbackUrl - Callback URL.
+    * @see https://www.wdonate.ru/documentation.html
+    * @see https://wdonate.ru/callback.html
+    */
+    setCallback (callbackUrl) {
+        return this.callApi('setCallback', { callbackUrl });
+    };
+
+    /**
+    * @name delCallback
+    * @description Метод для удаления текущего Callback URL.
+    * @see https://www.wdonate.ru/documentation.html
+    * @see https://wdonate.ru/callback.html
+    */
+    delCallback () {
+        return this.callApi('delCallback');
+    };
+};
